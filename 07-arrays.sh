51aeba73baf7225828333367b4530337d1f76653
sample <sample@gmail.com>
2025-05-04 20:10:15 +0530

    shell script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,6 +2,6 @@ MOVIES=("RRR" "Devara" "OG")
 
 echo "first name is: $MOVIES[0]"
 echo "second name is: $MOVIES[1]"
-echo "third name is: ${MOVIES[2]"
+echo "third name is: $MOVIES[2]"
 
 echo "all names: $MOVIES[@]"

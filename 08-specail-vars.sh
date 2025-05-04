8704b3561feb972f47a4067e05ef07508ad99cc2
sample <sample@gmail.com>
2025-05-04 20:19:24 +0530

    shell script

--- 08-specail-vars.sh
+++ 08-specail-vars.sh
@@ -7,4 +7,5 @@ echo "present working directory: $PWD"
 echo "home directory of current user: $HOME"
 echo "which user is running script: $USER"
 echo "process id of current script: $$"
+sleep 60 &
 echo "process id of last command: $!"

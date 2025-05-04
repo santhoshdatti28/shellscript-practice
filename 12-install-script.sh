f6ec11dcadd8093372950e4ea3e515096fd531b4
sample <sample@gmail.com>
2025-05-04 20:51:10 +0530

    shell script

---
+++ 12-install-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "error: you do not have access"
+    exit 1 
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "installing mysql is failed"
+    exit 1 
+else
+    echo "installing mysql is success"
+fi
+
+dnf install git -y 
+
+if [ $? -ne 0 ]
+then
+    echo "installing git is failed"
+else
+    echo "installing mysql is success"
+fi

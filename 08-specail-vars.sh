61b3c3ba340e01c8e0bbd6778086fc19257f2242
sample <sample@gmail.com>
2025-05-04 20:15:50 +0530

    shell script

---
+++ 08-specail-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "all variables passed: $@"
+echo "number of variables passed: $#"
+echo "script name: $0"
+echo "present working directory: $PWD"
+echo "home directory of current user: $HOME"
+echo "which user is running script: $USER"
+echo "process id of current script: $$"
+echo "process id of last command: $!"

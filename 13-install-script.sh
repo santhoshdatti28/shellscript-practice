537fc069dfa95d4ab9c8bca48ce7a0339311027e
sample <sample@gmail.com>
2025-05-04 20:57:38 +0530

    shell script

---
+++ 13-install-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error: you do not have access"
+    exit 1 
+fi 
+
+dnf list installed httpd
+
+if [ $? -ne 0 ]
+then
+    dnf install httpd
+    if [ $? -ne 0 ]
+    then
+        echo "installing httpd is failed"
+        exit 1
+    else
+        echo "installing httpd is success"
+    fi
+else
+    echo "httpd is already installed"
+fi
+

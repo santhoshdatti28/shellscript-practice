323ecde2b1fc727db843556d2742061bce82a65c
sample <sample@gmail.com>
2025-05-05 13:04:49 +0530

    shell script

---
+++ 15-colours.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m" 
+N="\e[0m"
+
+USERID=$(id -u )
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Error: you do not have sudo access"
+    exit 1 
+fi 
+
+VALIDATION(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is....$R failed $N"
+        exit 1
+    else
+        echo "$2 is...$G success $N"
+}
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y 
+    VALIDATION $? "installing mysql"
+fi 
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    dnf install git -y 
+    VALIDATION $? "installing git"
+else
+    echo "git is already...$Y installed $N"
+fi

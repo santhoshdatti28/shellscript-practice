d102d90b095ec326fcae8f9152ede08af3563971
sample <sample@gmail.com>
2025-05-06 00:22:52 +0530

    shell script

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript_logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+SOURCE_DIR="/home/ec2-user/app-logs"
+FILES_TO_DELETE=$(find /home/ec2-user/app-logs -name "*.log" -mtime +14)
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then 
+        echo "Error: you do nothave access"
+    fi
+}
+
+CHECK_ROOT
+
+mkdir -p /var/log/shellscript_logs
+
+echo "script started and executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+echo "files to delete: $FILES_TO_DELETE"

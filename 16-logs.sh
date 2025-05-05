acd3cedba86ab204f495fa340b6c85e7e7d1d260
sample <sample@gmail.com>
2025-05-05 14:14:35 +0530

    shell script

--- 16-logs.sh
+++ 16-logs.sh
@@ -10,10 +10,37 @@ LOGS_FOLDER= "/var/log/shellscript_logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-# VALIDATION(){
+ VALIDATION(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...failure"
+        exit 1
+    else
+        echo "$2...success"
+    fi
 
-# }
+ }
 
 echo "script started and executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATION $? "installing mysql"
+else
+    echo "mysql is already installed"
+fi 
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATION $? "installing git"
+else
+    echo "git is already installed"
+fi

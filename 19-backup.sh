c1c008714913dc7e82cc6b1b7cfc3045217c8e3e
sample <sample@gmail.com>
2025-05-06 13:11:39 +0530

    shell script

---
+++ 19-backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/home/ec2-user/shell-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+USAGE(){
+    echo -e "$R USAGE: $N sh 19-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(OPTIONAL)>"
+    exit 1
+}
+
+
+mkdir -p /home/ec2-user/shell-logs
+
+if [ $# -lt 2 ]
+then 
+    USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$R SOURCE DIR does exist..please check $N"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then 
+    echo -e "$R DEST DIR does not exist..please check $N"
+    exit 1
+fi
+
+FILES=$(find /home/ec2-user/app-logs -name "*.log" -mtime +$DAYS)
+
+echo "files to be: $FILES"
